c215dbb5e16e11bb5a18021145d0f0ebd98eda70
dawitgizachew <132124207+dawitgizachew@users.noreply.github.com>
2024-05-06 14:05:06 +0300

    a
    a

---
+++ Makefile
@@ -0,0 +1,44 @@
+PYTHON = python
+
+.PHONY: usage
+usage:
+	@echo 'Usage: make [target]'
+	@echo ''
+	@echo 'Targets:'
+	@echo '  black         Auto format Python code'
+	@echo '  isort         Fix isort issues'
+	@echo '  po            (Re)generate .po files'
+	@echo '  mo            Compile .po into .mo'
+	@echo '  qa            Perform QA checks'
+	@echo '  test          Execute test suite'
+
+.PHONY: po
+po:
+	( cd allauth ; $(PYTHON) ../manage.py makemessages -a -e html,txt,py )
+
+.PHONY: mo
+mo:
+	( cd allauth ; $(PYTHON) ../manage.py compilemessages )
+
+.PHONY: isort
+isort:
+	isort .
+
+.PHONY: black
+black:
+	black allauth/ setup.py
+
+.PHONY: test
+test:
+	pytest allauth/
+
+.PHONY: qa
+qa: validate-api-spec
+	flake8 allauth
+	isort --check-only --diff .
+	black --check .
+	djlint --check allauth examples
+
+.PHONY:
+validate-api-spec:
+	swagger-cli validate docs/headless/openapi-specification/openapi.yaml
---
+++ shell.nix
@@ -0,0 +1,39 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+    name = "django-allauth";
+    buildInputs = [
+        black
+        gettext
+        isort
+        djlint
+        python310
+        python310Packages.django
+        python310Packages.flake8
+        python310Packages.debugpy
+        python310Packages.pycodestyle
+        python310Packages.pyls-flake8
+        python310Packages.pylsp-rope
+        python310Packages.pytest
+        python310Packages.pytest-cov
+        python310Packages.pytest-django
+        python310Packages.python-lsp-server
+        python310Packages.python3-openid
+        python310Packages.python3-saml
+        python310Packages.qrcode
+        python310Packages.sphinx-rtd-theme
+        python310Packages.requests-oauthlib
+        python310Packages.tox
+        python310Packages.daphne
+        sphinx
+        twine
+
+        nodejs
+        playwright-test
+
+        swagger-cli
+    ];
+    shellHook = ''
+        export PATH="$PWD/node_modules/.bin/:$PATH"
+    '';
+}
